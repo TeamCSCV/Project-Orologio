ce066c943be7f5c1cd2a63badcb84ffb8d6d4f0b
SenuriMR <senuri.ranatunga98@gmail.com>
2021-06-05 12:51:26 +0530

    Modified the keyinput library for the final

---
+++ Group/AVR C++ Libraries/Keypad/keyInput/KeyIn/Key_Pad.cpp
@@ -0,0 +1,46 @@
+﻿/*
+ * Key_Pad.cpp
+ *
+ * Created: 2021-04-20 8:39:10 PM
+ * Author : Senuri Ranatunga
+ */ 
+
+#include "Key_Pad.h"
+char keys[4][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'},{'*','0','#'}};
+
+//Scanning the keypad repeatedly to check whether a key is pressed
+char Key_Pad :: keyInput(){
+	while (1){
+		KEYROW_PORT |= 0b00001111; //set all row pins to HIGH
+		for (int r=0; r<=3; r++){
+			KEYROW_DDR &= ~0b00001111; //clearing previous bits
+			KEYCOL_DDR &= ~0b00000111; //clearing previous bits
+			KEYROW_DDR |= 0b00000001 << r ; //set the current pin to output
+			KEYROW_PORT &= ~(0b00000001 << r); // set the current row pin to LOW
+			for (int c=0; c<=2; c++){
+				KEYCOL_PORT |= (0b00000001 << c);
+				if (!(KEYCOL_PIN & (0b00000001 << c))){ // check each column in the respective row for a pressed key
+					char pressed = keys[r][c]; // obtain the pressed key
+					if ((pressed!='*') & (pressed!='#')){
+						return pressed;
+					}
+					return 'X';
+				}
+			}
+		}
+	return 'X';}
+}
+
+char Key_Pad :: pressedKey(){
+	while (1){
+	char key = keyInput();
+	if (key != 'X'){
+		return key; // if a number is pressed return the pressed key
+	 }
+	}
+}
+
+
+
+
+
